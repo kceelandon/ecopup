8952765ef0af5cc3a944604abab448a09f2bc0c2
Kcee Landon <landonkcee@gmail.com>
2021-05-17 19:17:15 -0700

    put the map in and some other dependencies

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} initialRegion={{
+        latitude: 43.0766,
+        longitude: 89.4125,
+        latitudeDelta: 0.04,
+        longitudeDelta: 0.05,
+      }}/>
     </View>
   );
 }
@@ -17,5 +22,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
